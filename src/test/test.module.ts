2ccbbe249a8255f58c4519cc6d91cc978d68fb7a
Pavel Strunkin <pashidlos@gmail.com>
2020-05-03 11:29:41 +0200

    test module fixed

--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -3,9 +3,10 @@ import { TestService } from './test.service';
 import { TestController } from './test.controller';
 import { TestRunsModule } from 'src/test-runs/test-runs.module';
 import { TestVariationsModule } from 'src/test-variations/test-variations.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TestRunsModule, TestVariationsModule],
+  imports: [TestRunsModule, TestVariationsModule, UsersModule],
   providers: [TestService],
   controllers: [TestController],
   exports: [TestService]
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { getModelToken } from '@nestjs/sequelize';
 
 describe('UsersService', () => {
   let service: UsersService;
