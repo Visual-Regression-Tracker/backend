4193f0f13bbd37968078a4c3a4cb52db0acd85fa
Pavel Strunkin <pashidlos@gmail.com>
2020-05-22 22:44:19 +0200

    variation controller. Get  method added

--- src/test-variations/test-variations.controller.ts
+++ src/test-variations/test-variations.controller.ts
@@ -1,9 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, ParseUUIDPipe, Get, UseGuards, Param, Query } from '@nestjs/common';
+import { ApiTags, ApiParam, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { TestVariationsService } from './test-variations.service';
+import { TestVariation } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @ApiTags('test-variations')
 @Controller('test-variations')
 export class TestVariationsController {
-  constructor(private testVariations: TestVariationsService) {}
+  constructor(private testVariations: TestVariationsService, private prismaService: PrismaService) { }
+
+  @Get()
+  @ApiQuery({ name: 'projectId', required: true })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  getList(
+    @Query('projectId', new ParseUUIDPipe()) projectId,
+  ): Promise<TestVariation[]> {
+    return this.prismaService.testVariation.findMany({
+      where: { projectId }
+    });
+  }
 }
