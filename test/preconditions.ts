d45c8452f8d067a7c4117ffcdda1ad0116dcca37
Pavel Strunkin <pashidlos@gmail.com>
2020-05-23 23:40:25 +0200

    projects. e2e tests added

--- test/preconditions.ts
+++ test/preconditions.ts
@@ -2,10 +2,10 @@ import { INestApplication } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { UserLoginRequestDto } from 'src/users/dto/user-login-request.dto';
 import uuidAPIKey from 'uuid-apikey';
-import request, { Test } from 'supertest';
+import request, { Test, CallbackHandler, Request } from 'supertest';
 import { CreateUserDto } from 'src/users/dto/user-create.dto';
 
-export const generateUser = (password: string = '123456'): { email: string, password: string, firstName: string, lastName: string } => ({
+export const generateUser = (password: string): { email: string, password: string, firstName: string, lastName: string } => ({
     email: `${uuidAPIKey.create().uuid}@example.com'`,
     password,
     firstName: 'fName',
@@ -18,13 +18,12 @@ export const requestWithAuth = (app: INestApplication, method: 'post' | 'get' | 
         .set('Authorization', 'Bearer ' + token)
         .send(body)
 
-// export const haveUserCreated = (usersService: UsersService, password: string = '123456') => {
-//     return usersService.create({
-//         email: `${uuidAPIKey.create().uuid}@example.com'`,
-//         password,
-//         firstName: 'fName',
-//         lastName: 'lName',
-//     })
-// }
+export const haveUserLogged = async (usersService: UsersService) => {
+    const password = '123456'
+    const user = await usersService.create(generateUser(password))
 
-// export const haveUserLogged = (usersService: UsersService, user: UserLoginRequestDto) => usersService.login(user)
+    return usersService.login({
+        email: user.email,
+        password
+    })
+}
