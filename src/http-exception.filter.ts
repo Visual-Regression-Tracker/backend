2815930f61a8a0b4be7ebcf89eeb799c2242b22d
Pavel Strunkin <pashidlos@gmail.com>
2020-09-10 17:41:37 +0200

    Update http-exception.filter.ts

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
