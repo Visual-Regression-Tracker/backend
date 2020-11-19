c649c0463d1893824c2cbfc7475c7f5244e4fd4e
Pavel Strunkin <pashidlos@gmail.com>
2020-11-19 23:24:01 +0200

    https config added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
 import * as bodyParser from 'body-parser';
+import { readFileSync } from 'fs';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+
+function getHttpsOptions(): HttpsOptions | null {
+  if (!process.env.HTTPS_KEY_PATH || !process.env.HTTPS_CERT_PATH) {
+    Logger.log('HTTPS is not configured');
+    return null;
+  }
+  let cert: Buffer, key: Buffer;
+  try {
+    key = readFileSync(process.env.HTTPS_KEY_PATH);
+  } catch (err) {
+    Logger.error('HTTPS_KEY_PATH is not correct', err.stack);
+  }
+  try {
+    cert = readFileSync(process.env.HTTPS_CERT_PATH);
+  } catch (err) {
+    Logger.error('HTTPS_CERT_PATH is not correct', err.stack);
+  }
+  return {
+    key,
+    cert,
+  };
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions: getHttpsOptions(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
 
@@ -16,11 +43,7 @@ async function bootstrap() {
   }
 
   // serve images
-  app.use(
-    express.static(
-      join(process.cwd(), process.env.IMG_UPLOAD_FOLDER || 'imageUploads/')
-    )
-  );
+  app.use(express.static(join(process.cwd(), process.env.IMG_UPLOAD_FOLDER || 'imageUploads/')));
 
   await app.listen(process.env.APP_PORT || 3000);
 }
