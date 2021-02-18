d46a381cfa5044ce79090d518f2b3ca251f246cb
Surat Das <surat.das@gmail.com>
2021-02-17 23:34:21 -0800

    Fixed review comments.

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -9,20 +9,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
 
     let status: number;
-    let message = exception.message;
-    if (message.includes("Unique constraint failed on the fields")) {
-      message = (message.includes("build.update()")) ? "There is already a build with this ci build id."
-        : (message.includes("project.create()")) ? "Project exists with this name."
-          : (message.includes("user.create()")) ? "This user already exists." : message;
-
-      status = HttpStatus.BAD_REQUEST
-    } else {
-      try {
-        status = exception.getStatus();
-      } catch {
-        status = HttpStatus.INTERNAL_SERVER_ERROR;
-      }
+    const isUniqueConstaintException = this.isUniqueConstraintException(exception);
+    try {
+      status = isUniqueConstaintException ? HttpStatus.BAD_REQUEST : exception.getStatus();
+    } catch {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
+    const message = isUniqueConstaintException ? this.getCustomMessageForException(exception) : exception.message;
 
     Logger.error(exception, exception.stack);
 
@@ -34,4 +27,17 @@ export class HttpExceptionFilter implements ExceptionFilter {
       stack: exception.stack,
     });
   }
+
+  isUniqueConstraintException(exception: HttpException): boolean {
+    return exception.message.includes("Unique constraint failed on the fields");
+  }
+
+  getCustomMessageForException(exception: HttpException): string {
+    let message = exception.message;
+    message = (message.includes("build.update()")) ? "There is already a build with this ci build id."
+      : (message.includes("project.create()")) ? "Project exists with this name."
+        : (message.includes("user.create()")) ? "This user already exists." : message;
+    return message;
+  }
+
 }
