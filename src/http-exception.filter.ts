e3ac6fbf2f1b92c6ee662f50cd066f9b089ce8df
Surat Das <surat.das@gmail.com>
2021-08-22 11:53:19 -0700

    Add clearer error message for unauthorized user (#159)
    * Add clearer error message for unauthorized user
    
    Fixes part of the issue: https://github.com/Visual-Regression-Tracker/Visual-Regression-Tracker/issues/296
    
    * Renamed the method to a more relevant one.

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -10,12 +10,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     let status: number;
     const isUniqueConstaintException = this.isUniqueConstraintException(exception);
+    const isForbiddenException = this.isForbiddenException(exception);
     try {
       status = isUniqueConstaintException ? HttpStatus.BAD_REQUEST : exception.getStatus();
     } catch {
       status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
-    const message = isUniqueConstaintException ? this.getCustomMessageForException(exception) : exception.message;
+    const message = (isUniqueConstaintException || isForbiddenException) ? this.getCustomMessageForException(exception) : exception.message;
 
     Logger.error(exception, exception.stack);
 
@@ -28,6 +29,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
     });
   }
 
+  isForbiddenException(exception: HttpException): boolean {
+    return exception.message.includes("Forbidden resource");
+  }
+
   isUniqueConstraintException(exception: HttpException): boolean {
     return exception.message.includes("Unique constraint failed on the fields");
   }
@@ -36,7 +41,8 @@ export class HttpExceptionFilter implements ExceptionFilter {
     let message = exception.message;
     message = (message.includes("build.update()")) ? "There is already a build with this ci build id."
       : (message.includes("project.create()")) ? "Project exists with this name."
-        : (message.includes("user.create()")) ? "This user already exists." : message;
+        : (message.includes("user.create()")) ? "This user already exists."
+          : (message.includes("Forbidden resource")) ? "You do not have permission to perform this operation." : message;
     return message;
   }
 
