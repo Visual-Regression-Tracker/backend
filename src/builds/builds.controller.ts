3b261f3704a6fa2f5d0f850db43754039cde93fe
Surat Das <surat.das@gmail.com>
2021-12-22 13:31:54 -0800

    If branch not sent, take default branch

--- src/builds/builds.controller.ts
+++ src/builds/builds.controller.ts
@@ -75,7 +75,7 @@ export class BuildsController {
     await this.buildsService.deleteOldBuilds(project.id, project.maxBuildAllowed);
     const build = await this.buildsService.findOrCreate({
       projectId: project.id,
-      branchName: createBuildDto.branchName,
+      branchName: (createBuildDto.branchName && createBuildDto.branchName.trim().length > 0) ? createBuildDto.branchName : project.mainBranchName,
       ciBuildId: createBuildDto.ciBuildId,
     });
     return new BuildDto(build);
