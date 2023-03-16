7d66551cf6b194afc55d6fa5704c6e8245abdba1
Mario Melcher <mario.melcher@seitenbau.com>
2023-03-16 10:01:52 +0100

    fix string parsing from environment variable

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { Server } from 'http';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
+  private readonly logger: Logger = new Logger(AppService.name);
+
   constructor(
     private readonly refHost: HttpAdapterHost<any>,
     @Inject(ConfigService) private configService: ConfigService
@@ -13,8 +15,11 @@ export class AppService implements OnApplicationBootstrap {
   onApplicationBootstrap() {
     const server: Server = this.refHost.httpAdapter.getHttpServer();
     const { timeout, headersTimeout, keepAliveTimeout } = server;
-    server.timeout = this.configService.get<number>('SERVER_TIMEOUT', timeout);
-    server.headersTimeout = this.configService.get<number>('SERVER_HEADERS_TIMEOUT', headersTimeout);
-    server.keepAliveTimeout = this.configService.get<number>('SERVER_KEEP_ALIVE_TIMEOUT', keepAliveTimeout);
+    server.timeout = parseInt(this.configService.get('SERVER_TIMEOUT', timeout.toString()));
+    this.logger.log(`server.timeout is ${server.timeout}`);
+    server.headersTimeout = parseInt(this.configService.get('SERVER_HEADERS_TIMEOUT', headersTimeout.toString()));
+    this.logger.log(`server.headersTimeout is ${server.headersTimeout}`);
+    server.keepAliveTimeout = parseInt(this.configService.get('SERVER_KEEP_ALIVE_TIMEOUT', keepAliveTimeout.toString()));
+    this.logger.log(`server.keepAliveTimeout is ${server.keepAliveTimeout}`);
   }
 }
